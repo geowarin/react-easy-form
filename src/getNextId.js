87fb819c853ad90a5643bda088e64dd79ef1a2eb
geowarin <geowarin@gmail.com>
2015-11-14 15:24:28 +0100

    remove debug

--- src/getNextId.js
+++ src/getNextId.js
@@ -1,7 +1,5 @@
 let currentId = 1;
 
 export default function getNextId(prefix = 'id_') {
-  const id = prefix + (currentId++);
-  console.log('nextId', id)
-  return id;
+  return prefix + (currentId++);
 }
